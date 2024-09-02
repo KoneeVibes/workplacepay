d6e1eff0f52431f91722d73d3c1740b82918beea
Newton <ephraimnewton533@gmail.com>
2024-09-02 16:04:52 +0100

    newton

--- src/containers/home/hero/index.js
+++ src/containers/home/hero/index.js
@@ -1,7 +1,6 @@
 import { BaseButton } from "../../../components/buttons";
 import { HeroWrapper } from "./styled";
 // import { HeroBaseButton } from "./styled";
-import heroImage from "../../../assets/images/heroImage.jpeg";
 
 export const Hero = () => {
   return (
@@ -18,26 +17,6 @@ export const Hero = () => {
         <div className="hero-button">
           <BaseButton backgroundColor={"#4E57BB"}>Get Started</BaseButton>
           <BaseButton>Login</BaseButton>
-        </div>
-      </div>
-
-      <div className="second-hero">
-        <img
-          src={heroImage}
-          alt="a picture representing diplomatic agreement"
-        />
-        <div className="second-hero-text-section">
-          <h2>why workPlacePay?</h2>
-          <h3>Empower your business with seamless payroll management</h3>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus in
-            sem orci. Nunc a sagittis lacus. Nunc turpis augue, sodales quis
-            fermentum vitae, vestibulum a orci. Donec vitae ultrices odio.
-            Mauris condimentum id arcu sit amet venenatis. Quisque vitae nisi
-            egestas, varius lorem eget, aliquam neque. Donec ornare egestas
-            lacinia. Nulla pellentesque eleifend ullamcorper.
-          </p>
-          <BaseButton>Get Started</BaseButton>
         </div>
       </div>
     </HeroWrapper>
--- src/containers/home/whyworkplacepay/index.js
+++ src/containers/home/whyworkplacepay/index.js
@@ -1,10 +1,27 @@
 import { WhyWorkPlacePayWrapper } from "./styled";
+import { BaseButton } from "../../../components/buttons";
+import heroImage from "../../../assets/images/heroImage.jpeg";
 
 export const WhyWorkPlacePay = () => {
-    return (
-        <WhyWorkPlacePayWrapper>
-            {/* Ibukun your html code should begin below this line */}
-
-        </WhyWorkPlacePayWrapper>
-    )
-}
+  return (
+    <WhyWorkPlacePayWrapper>
+      {/* Ibukun your html code should begin below this line */}
+      <div className="second-hero">
+        <img src={heroImage} alt="diplomatic agreement" />
+        <div className="second-hero-text-section">
+          <h2>why workPlacePay?</h2>
+          <h3>Empower your business with seamless payroll management</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus in
+            sem orci. Nunc a sagittis lacus. Nunc turpis augue, sodales quis
+            fermentum vitae, vestibulum a orci. Donec vitae ultrices odio.
+            Mauris condimentum id arcu sit amet venenatis. Quisque vitae nisi
+            egestas, varius lorem eget, aliquam neque. Donec ornare egestas
+            lacinia. Nulla pellentesque eleifend ullamcorper.
+          </p>
+          <BaseButton>Get Started</BaseButton>
+        </div>
+      </div>
+    </WhyWorkPlacePayWrapper>
+  );
+};
