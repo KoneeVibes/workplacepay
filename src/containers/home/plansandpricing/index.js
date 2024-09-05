d14f5b141f81f9501ae270b8d9a9705608cfe2c2
Ibukun <ibukunoladiporaji@gmail.com>
2024-09-05 20:54:18 +0100

    Update on plans and pricing

--- src/containers/home/plansandpricing/index.js
+++ src/containers/home/plansandpricing/index.js
@@ -1,10 +1,25 @@
-import { PlansAndPricingWrapper } from "./styled";
+import { H2, H3, } from "../../../components/typograpy/styled";
+import { PlansAndPricingRow, PlansAndPricingWrapper } from "./styled";
 
 export const PlansAndPricing = () => {
-  return (
-    <PlansAndPricingWrapper>
-      {/* Awele, your html code should begin below this line */}
-      <h1>Plans and Pricing</h1>
-    </PlansAndPricingWrapper>
-  );
-};
+    return (
+        <PlansAndPricingWrapper>
+            {/* Awele, your html code should begin below this line */}
+                <H2>Plans and Pricing</H2>
+                <PlansAndPricingRow className="pricingRow"
+                    tocolumn={true}
+                >
+                    <div className='bronze'>
+                        <H3>Bronze</H3>   
+
+                    </div>
+                    <div className='silver'>
+                        <H3>Silver</H3>
+                    </div>
+                    <div className='gold'>
+                        <H3>Gold</H3>
+                    </div>
+                </PlansAndPricingRow>
+        </PlansAndPricingWrapper>
+    )
+}
--- src/containers/home/plansandpricing/styled.js
+++ src/containers/home/plansandpricing/styled.js
@@ -1,8 +1,39 @@
 import styled from "styled-components";
+import { Row } from "../../../components/flex/styled";
 
-export const PlansAndPricingWrapper = styled("div")(() => {
-    return {
-        // Awele, your CSS styling should begin below this line
+export const PlansAndPricingWrapper = styled('div')(() => ({
+    // Awele, your CSS styling should begin below this line
+    padding: "var(--cardPadding) var(--pagePadding)",
+    "h2":{
+        textAlign: "center",
+    },
+    ".pricingRow":{
+        overflow:"hidden",
+    },
+    '.bronze': {
+        flex: 1,
+        backgroundColor: '#D3D5F0',
+        borderRadius: "1rem",
+        padding: "var(--cardPadding)",
+    },
+        
+    ".silver": {
+        flex: 1,
+        backgroundColor: '#8A90DC',
+        borderRadius: "1rem",
+        padding: "var(--cardPadding)",
+    },
 
+    ".gold":{
+        flex: 1,
+        backgroundColor: '#D3D5F0',
+        borderRadius: "1rem",
+        padding: "var(--cardPadding)"
+    },
+}));
+
+export const PlansAndPricingRow = styled(Row)(() => {
+    return{
+        justifyContent: "space-between"
     }
 })
