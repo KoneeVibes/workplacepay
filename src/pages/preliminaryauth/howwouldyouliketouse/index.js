4fc8b168cacc0f835da6390996f612c07d909acd
Your Name <mimijoseph111@gmail.com>
2024-09-13 14:11:13 +0100

    created one preliminary page

--- src/pages/preliminaryauth/howwouldyouliketouse/index.js
+++ src/pages/preliminaryauth/howwouldyouliketouse/index.js
@@ -1,11 +1,31 @@
-import { H1 } from "../../../components/typography/styled"
-import { HowWouldYouLikeToUseWrapper } from "./styled"
+import { Prelim } from "../../../assets";
+import { Column } from "../../../components/flex/styled";
+import { BaseButton } from "../../../components/buttons";
+import { H1, H3, P } from "../../../components/typography/styled";
+import { HowWouldYouLikeToUseWrapper } from "./styled";
 
 export const HowWouldYouLikeToUse = () => {
-    return (
-        <HowWouldYouLikeToUseWrapper>
-            {/* Mirabel, your HTML should go under here */}
-            <H1>How would you like to use page</H1>
-        </HowWouldYouLikeToUseWrapper>
-    )
-}
+  return (
+    <HowWouldYouLikeToUseWrapper tocolumn={true}>
+      {/* Mirabel, your HTML should go under here */}
+      <div className="image-page">
+        <H1>WorkPlacePay</H1>
+        <H3>A few clicks from creating your account </H3>
+        <P>Seamless Payroll management for your business all in one place!</P>
+        <Prelim />
+      </div>
+      <Column className="basebutton-page">
+        <P>
+          How would you like to use workPlacePAY You can create your company
+          account right away if you're an employer or refer your employer if
+          you're an employee.
+        </P>
+        <Column className="basebutton">
+        <BaseButton>I am an Employer</BaseButton>
+
+          <BaseButton>I want to refer my Employer</BaseButton>
+          </Column>
+      </Column>
+    </HowWouldYouLikeToUseWrapper>
+  );
+};
--- src/pages/preliminaryauth/howwouldyouliketouse/styled.js
+++ src/pages/preliminaryauth/howwouldyouliketouse/styled.js
@@ -1,8 +1,32 @@
 import styled from "styled-components";
+import { Row } from "../../../components/flex/styled";
 
-export const HowWouldYouLikeToUseWrapper = styled("div")(() => {
-    return {
-        // Mirabel, your CSS should go under here
-        
-    }
-})
+export const HowWouldYouLikeToUseWrapper = styled(Row)(() => {
+  return {
+    // Mirabel, your CSS should go under here
+    gap: 0,
+    justifyContent: "space-between",
+    ".image-page, .basebutton-page": {
+      flex: 1,
+    },
+
+    ".image-page": {
+      padding: "calc(var(--cardPadding) * 2) calc(var(--cardPadding) * 4)",
+      backgroundColor: "#4E57BB",
+      color: "white",
+    },
+
+    ".image-page>svg": {
+      width: "100%",
+    },
+
+    ".basebutton-page": {
+      padding: "calc(var(--cardPadding) * 2) calc(var(--cardPadding) * 4)",
+      margintop: "calc(var(--sectionMargin) ) ",
+    }, 
+
+    ".basebutton": {
+      gap: "calc(var(--flexGap) * 4)",
+    },
+  };
+});
