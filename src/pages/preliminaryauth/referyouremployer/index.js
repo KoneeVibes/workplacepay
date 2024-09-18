30a01eeace90309e12afdab557e41a28bd5f4ff8
Ibukun <ibukunoladiporaji@gmail.com>
2024-09-18 09:51:39 +0100

    Update on Refer your Employer

--- src/pages/preliminaryauth/referyouremployer/index.js
+++ src/pages/preliminaryauth/referyouremployer/index.js
@@ -1,28 +1,30 @@
-import { Column } from "../../../components/flex/styled";
+import { useState } from "react";
 import { H1, H2, H3, P, Label } from "../../../components/typography/styled";
 import { ReferYourEmployerWrapper, ReferYourEmployerRow } from "./styled";
+import { BaseFieldSet } from "../../../components/form/fieldset/styled";
 import { BaseInput } from "../../../components/form/input/styled";
-import refereImg from "../../../assets/images/refereImg.png" ;
+import { PrelimSetup } from "../../../assets";
+import { BaseButton } from "../../../components/button/styled";
 
 export const ReferYourEmployer = () => {
     return (
         <ReferYourEmployerWrapper tocolumn={true}>
             {/* IBK, your HTML should go under here */}
-            <Column className="refer-text">
-            <H2>workPlacePAY</H2>
-            <H3>The #1 Employee 
-            Payroll Solution for
-            Small and medium Businesses</H3>
-            <P>Seamless Payroll management for your buisnessall in one place!</P>
-            <img src={refereImg} alt="Refere Images" className="refere-img"/>
-            </Column>
+            <div className="refer-text">
+                <H2>workPlacePAY</H2>
+                <H3>The #1 Employee 
+                    Payroll Solution for
+                    Small and medium Businesses
+                </H3>
+                <P>Seamless Payroll management for your buisnessall in one place!</P>
+                <PrelimSetup />
+            </div>
             <div className="refer-form">
                 <H1>Refer your Employer </H1>
                 <P>Please  provide the following details to refer your employer to workPlacePAY</P>
-                <ReferYourEmployerRow
-                  tocolumn={true}
-                >
-                    <div>
+                <form> 
+                  <ReferYourEmployerRow>
+                     <BaseFieldSet>
                         <Label>Firstname</Label>
                         <BaseInput
                         type="text"
@@ -30,8 +32,8 @@ export const ReferYourEmployer = () => {
                         placeholder="Enter First Name"
                         required
                         />
-                    </div>
-                    <div>
+                     </BaseFieldSet>
+                     <BaseFieldSet>
                         <Label>Lastname</Label>
                         <BaseInput
                         type="text"
@@ -39,12 +41,10 @@ export const ReferYourEmployer = () => {
                         placeholder="Enter Last Name"
                         required
                         />
-                    </div>
+                    </BaseFieldSet>
                 </ReferYourEmployerRow>
-                <ReferYourEmployerRow
-                  tocolumn={true}
-                >
-                    <div>
+                <ReferYourEmployerRow>
+                    <BaseFieldSet>
                         <Label color="">Company name*</Label>
                         <BaseInput
                         type="text"
@@ -52,8 +52,8 @@ export const ReferYourEmployer = () => {
                         placeholder="Company Name"
                         required
                         />
-                    </div>
-                    <div>
+                    </BaseFieldSet>
+                    <BaseFieldSet>
                         <Label>Company Email</Label>
                         <BaseInput
                         type="email"
@@ -61,8 +61,12 @@ export const ReferYourEmployer = () => {
                         placeholder="Company Email"
                         required
                         />
-                    </div>
+                    </BaseFieldSet>
                 </ReferYourEmployerRow>
+                <BaseButton backgroundcolor={"#4E57BB"} width={"fit-content"} className="submit-button">
+                   Submit
+                </BaseButton>
+            </form>    
             </div>
         </ReferYourEmployerWrapper>
     )
--- src/pages/preliminaryauth/referyouremployer/styled.js
+++ src/pages/preliminaryauth/referyouremployer/styled.js
@@ -4,39 +4,80 @@ import { Row } from "../../../components/flex/styled";
 export const ReferYourEmployerWrapper = styled(Row)(() => {
     return {
         // IBK, your CSS should go under here
-        // padding: "0 var(--pagePadding)",
-        // margin: "calc(var(--sectionMargin) * 2) 0",
         gap: 0,
-        ".refer-text": {
-            flex: 0.4,
+        "& .refer-text": {
+            flex: 1,
             backgroundColor: "#4E57BB",
-            textAlign:"center",
-            color:"#ffffff",
+            color: "#ffffff",
+            padding: "calc(var(--cardPadding) * 2) ",
+            overflow: "hidden",
         },
-        ".refer-form": {
-            flex: 0.6,
-            justifyContent:"center",
-            marginTop: "calc(var(--sectionMargin) * 3)",
+        "& .refer-form": {
+            flex: 1,
+            backgroundColor: "#ffffff",
+            padding: "calc(var(--cardPadding) * 2)",
+
         },
-        ".refere-img": {
-           width: "100%",
-           height: "auto",
-           marginTop: "calc(var(--sectionMargin) * 3)",
+        "& form": {
+            display: "flex",
+            flexDirection: "column",
+            gap: "calc(var(--flexGap) * 2)",
+            marginBlock: "calc(var(--sectionMargin) * 2)",
         },
-        ".refer-text>h2, .refer-text>h3, .refer-text>p ": {
-            // marginTop: "var(--sectionMargin)",
+        "& .refer-text>svg": {
+            width: "100%",
+            height: "auto",
         },
-        "@media screen and (min-width: 1024px)": {
-          margin: "calc(var(--sectionMargin) * 3) 0",
+        "& .refer-form fieldset": {
+            flex: 1,
+            overflow: "hidden",
+            display: "flex",
+            gap: "calc(var(--flexGap)/8)",
+            flexDirection: "column",
+            justifyContent: "space-between",
+        },
+        "& .refer-form label": {
+            fontFamily: "Poppins",
+            fontWeight: 500,
+            color: "#000000"
+        },
+        "& .refer-form input, & .form-container select": {
+            padding: "calc(var(--cardPadding) / 2) var(--cardPadding)",
+            outline: "none",
+            border: "none",
+            backgroundColor: "#F2F2F8",
+            borderRadius: "8px",
+        },
+        "@media screen and (max-width: 425px)": {
+            "& .refer-text": {
+                padding: "calc(var(--cardPadding))",
+            },
+            "& .refer-form": {
+                padding: "calc(var(--cardPadding))",
+            },
+        },
+        "@media screen and (max-width: 1024px)": {
+            "& h2": {
+                marginBlock: 0,
+            },
+            "& form": {
+                gap: "var(--flexGap)",
+            },
+        },
+        "@media screen and (min-width: 1440px)": {
+            "& .refer-form button": {
+                marginLeft: "auto",
+                marginTop: "calc(var(--sectionMargin))",
+                width: "fit-content",
+            }
         }
     }
 })
 
 export const ReferYourEmployerRow = styled(Row)(() => {
     return {
-        marginBottom:"calc(var(--sectionMargin) * 2)",
-        "BaseInput":{
-            gap: "calc(var(--flexGap)/2)",
+       "@media screen and (max-width: 768px)": {
+            flexDirection: "column",
         }
     }
 })
