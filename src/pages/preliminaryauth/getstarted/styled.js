3cc58d5de4b23153c573908804d25219d2a91816
Konee <104918313+KoneeVibes@users.noreply.github.com>
2024-09-23 16:54:27 +0100

    cleaned up get started page

--- src/pages/preliminaryauth/getstarted/styled.js
+++ src/pages/preliminaryauth/getstarted/styled.js
@@ -12,22 +12,15 @@ export const GetStartedWrapper = styled(Row)(() => {
       backgroundColor: "#4E57BB",
       color: "#FFFFFF",
     },
-    ".first-section>svg": {
+    "& .first-section>svg": {
       width: "100%",
       height: "auto",
     },
     "& form": {
-      display: "flex",
-      flexDirection: "column",
-      marginBlock: "calc(var(--sectionMargin) * 2)",
+      paddingBottom: "calc(var(--cardPadding))",
     },
-
-    "& .emailAddress-page fieldset": {
-      flex: 1,
-      display: "flex",
-      flexWrap: "wrap",
-      flexDirection: "column",
-      justifyContent: "space-between",
+    "& form>fieldset": {
+      overflow: "hidden",
     },
     "& .emailAddress-page": {
       flex: 1,
@@ -35,11 +28,13 @@ export const GetStartedWrapper = styled(Row)(() => {
       overflow: "hidden",
       padding: "calc(var(--cardPadding) * 2)",
     },
-
     "& .emailAddress-page label": {
       fontFamily: "Poppins",
       fontWeight: 500,
       color: "#000000",
+      lineHeight: "6rem",
+      display: "inline-block",
+      width: "100%",
     },
     "& input ": {
       padding: "calc(var(--cardPadding) / 2) var(--cardPadding)",
@@ -56,40 +51,18 @@ export const GetStartedWrapper = styled(Row)(() => {
         padding: "calc(var(--cardPadding))",
       },
     },
-    "@media screen and (max-width: 768px)": {
-      "& form": {
-        alignItems: "stretch",
-      },
-      "& .emailAddress-page input": {
-        width: "100%",
-      },
-      "& .emailAddress-page button": {
-        width: "100%",
-      },
-      "& .emailAddress-page": {
-        display: "flex",
-        flexDirection: "column", // Ensure vertical stacking
-      },
-    },
     "@media screen and (max-width: 1024px)": {
       "& h2": {
         marginBlock: 0,
-      },
-      "& form": {
-        gap: "var(--flexGap)",
-      },
-    },
-    "@media screen and (min-width: 1440px)": {
-      "& .emailAddress-page button": {
-        marginTop: "calc(var(--sectionMargin))",
-        width: "fit-content",
-      },
+      }
     },
   };
 });
 export const GetStartedWrapperRow = styled(Row)(() => {
   return {
-    "@media screen and (max-width: 768px)": {
+    gap: 0,
+    "@media screen and (max-width: 1280px)": {
+      gap: "calc(var(--flexGap)/1)",
       flexDirection: "column",
     },
   };
