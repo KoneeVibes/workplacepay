c67bc07eda28cad5244053f95a3199d70c937e8f
Newton <ephraimnewton533@gmail.com>
2024-10-03 19:45:51 +0100

    no vex say it took so long boss

--- src/containers/dashboard/payrollsettings/index.js
+++ src/containers/dashboard/payrollsettings/index.js
@@ -1,188 +1,437 @@
 import { Layout } from "../layout";
 import { PayrollSettingsWrapper } from "./styled";
-// import { BaseButton } from "../../../components/button/styled";
+import { BaseButton } from "../../../components/button/styled";
 import { BaseInput } from "../../../components/form/input/styled";
 
 export const PayrollSettings = () => {
   return (
     <Layout title={"Payroll Settings"}>
       <PayrollSettingsWrapper>
-        <div style={{ padding: "20px" }}>
-          <h1>Payroll Variables</h1>
-          <h2>Select the applicable variables for the user</h2>
+        <h1>Payroll Variables</h1>
+        <h2>Select the applicable variables for the user</h2>
 
-          <div
-            style={{
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "flex-start",
-            }}
-          >
-            <h2 style={{ color: "blue" }}>Earning</h2>
-            <form
+        <h2 style={{ color: "#4E57BB", fontSize: "2rem", fontWeight: "600" }}>
+          Earning
+        </h2>
+        <form>
+          <div className="flexRows">
+            <label className="label" htmlFor="Basic">
+              Basic
+            </label>
+            <div className="inputDivs">
+              <BaseInput
+                type="number"
+                className="basic"
+                defaultValue=""
+                style={{
+                  border: "1px solid black",
+                  padding: "0",
+                  borderRadius: "7px",
+                  width: "4rem",
+                  height: "1.5rem",
+                  marginLeft: "2.3rem",
+                }}
+              />
+              <span
+                style={{
+                  position: "absolute",
+                  left: "22%",
+                  pointerEvents: "none",
+                  background: "white",
+                  padding: ".1rem",
+                  border: "1px solid black",
+                  borderRight: "none",
+                }}
+              >
+                %
+              </span>
+            </div>
+            <div>
+              <BaseInput
+                type="radio"
+                id="option1"
+                name="option"
+                value="option1"
+                style={{
+                  marginLeft: "1.4rem",
+                }}
+                checked
+              />
+            </div>
+          </div>
+
+          <div className="flexRows">
+            <label className="label" htmlFor="housing">
+              Housing
+            </label>
+            <div
               style={{
+                position: "relative",
                 display: "flex",
-                flexDirection: "column",
-                alignItems: "flex-start",
-                gap: "1rem",
+                alignItems: "center",
               }}
             >
-              <div className="flexRows">
-                <label htmlFor="amount">Basic</label>
-                <div
-                  style={{
-                    position: "relative",
-                    display: "flex",
-                    alignItems: "center",
-                  }}
-                >
-                  <BaseInput
-                    type="number"
-                    id="amount"
-                    defaultValue=""
-                    style={{
-                      border: "1px solid black",
-                      padding: "0",
-                      borderRadius: "7px",
-                      width: "4rem",
-                    }}
-                  />
-                  <span
-                    style={{
-                      position: "absolute",
-                      right: "3%",
-                      pointerEvents: "none",
-                      background: "white",
-                      padding: "0 5px",
-                      borderLeft: "1px solid black",
-                    }}
-                  >
-                    %
-                  </span>
-                </div>
-                <div>
-                  <BaseInput
-                    type="radio"
-                    id="option1"
-                    name="option"
-                    value="option1"
-                    checked
-                  />
-                </div>
-              </div>
+              <select
+                id="options"
+                style={{
+                  border: "1px solid black",
+                  padding: "5px 5px 5px 0px",
+                  borderRight: "none",
+                  paddingRight: "10px",
+                  borderTopLeftRadius: "7px",
+                  borderBottomLeftRadius: "7px",
+                  marginLeft: ".5rem",
+                }}
+              >
+                <option value="" hidden></option>
+                <option value="option1">Option 1</option>
+                <option value="option2">Option 2</option>
+                <option value="option3">Option 3</option>
+              </select>
+              <span
+                style={{
+                  position: "absolute",
+                  right: "0",
+                  left: "100%",
+                  pointerEvents: "none",
+                  background: "white",
+                  padding: "3px 0",
+                  paddingLeft: ".2rem",
+                  paddingRight: "1.2rem",
+                  border: "1px solid black",
+                  borderTopRightRadius: "7px",
+                  borderBottomRightRadius: "7px",
+                }}
+              >
+                %
+              </span>
+            </div>
 
-              <div className="flexRows">
-                <label htmlFor="options">housing</label>
-                <div
-                  style={{
-                    position: "relative",
-                    display: "flex",
-                    alignItems: "center",
-                  }}
-                >
-                  <select
-                    id="options"
-                    style={{
-                      border: "1px solid black",
-                      padding: "5px 5px 5px 0px",
-                      borderRight: "none",
-                      // appearance: "none",
-                      paddingRight: "10px", // Space for the symbol
-                      borderTopLeftRadius: "7px",
-                      borderBottomLeftRadius: "7px",
-                    }}
-                  >
-                    <option value="option1">Option 1</option>
-                    <option value="option2">Option 2</option>
-                    <option value="option3">Option 3</option>
-                  </select>
-                  <span
-                    style={{
-                      position: "absolute",
-                      right: "0",
-                      left: "100%",
-                      pointerEvents: "none",
-                      background: "white",
-                      padding: "3px 0",
-                      paddingLeft: ".2rem",
-                      paddingRight: "1.2rem",
-                      border: "1px solid black",
-                      borderTopRightRadius: "7px",
-                      borderBottomRightRadius: "7px",
-                    }}
-                  >
-                    %
-                  </span>
-                </div>
-
-                <div>
-                  <input
-                    type="radio"
-                    id="option1"
-                    name="option"
-                    value="option1"
-                    checked
-                  />
-                </div>
-              </div>
-
-              <div className="flexRows">
-                <label htmlFor="options">Transport</label>
-                <div
-                  style={{
-                    position: "relative",
-                    display: "flex",
-                    alignItems: "center",
-                  }}
-                >
-                  <select
-                    id="options"
-                    style={{
-                      border: "1px solid black",
-                      padding: "5px 5px 5px 0px",
-                      borderRight: "none",
-                      // appearance: "none",
-                      paddingRight: "10px", // Space for the symbol
-                      borderTopLeftRadius: "7px",
-                      borderBottomLeftRadius: "7px",
-                    }}
-                  >
-                    <option value="option1"></option>
-                    <option value="option2">Option 2</option>
-                    <option value="option3">Option 3</option>
-                  </select>
-                  <span
-                    style={{
-                      position: "absolute",
-                      right: "0",
-                      left: "100%",
-                      pointerEvents: "none",
-                      background: "white",
-                      padding: "3px 0",
-                      paddingLeft: ".2rem",
-                      paddingRight: "1.2rem",
-                      border: "1px solid black",
-                      borderTopRightRadius: "7px",
-                      borderBottomRightRadius: "7px",
-                    }}
-                  >
-                    %
-                  </span>
-                </div>
-
-                <div>
-                  <input
-                    type="radio"
-                    id="option1"
-                    name="option"
-                    value="option1"
-                    checked
-                  />
-                </div>
-              </div>
-            </form>
+            <div>
+              <input
+                type="radio"
+                id="option1"
+                name="option"
+                value="option1"
+                checked
+              />
+            </div>
           </div>
-        </div>
+
+          <div className="flexRows">
+            <label className="label" htmlFor="Transport">
+              Transport
+            </label>
+            <div
+              style={{
+                position: "relative",
+                display: "flex",
+                alignItems: "center",
+              }}
+            >
+              <select
+                id="options"
+                style={{
+                  border: "1px solid black",
+                  padding: "5px 5px 5px 0px",
+                  borderRight: "none",
+                  // appearance: "none",
+                  paddingRight: "10px", // Space for the symbol
+                  borderTopLeftRadius: "7px",
+                  borderBottomLeftRadius: "7px",
+                }}
+              >
+                <option value="" hidden></option>
+                <option value="option1">Option 1</option>
+                <option value="option2">Option 2</option>
+                <option value="option3">Option 3</option>
+              </select>
+              <span
+                style={{
+                  position: "absolute",
+                  right: "0",
+                  left: "100%",
+                  pointerEvents: "none",
+                  background: "white",
+                  padding: "3px 0",
+                  paddingLeft: ".2rem",
+                  paddingRight: "1.2rem",
+                  border: "1px solid black",
+                  borderTopRightRadius: "7px",
+                  borderBottomRightRadius: "7px",
+                }}
+              >
+                %
+              </span>
+            </div>
+
+            <div>
+              <input
+                type="radio"
+                id="option1"
+                name="option"
+                value="option1"
+                checked
+              />
+            </div>
+          </div>
+          <div className="flexRows">
+            <label className="label" htmlFor="housing">
+              Housing
+            </label>
+            <div
+              style={{
+                position: "relative",
+                display: "flex",
+                alignItems: "center",
+              }}
+            >
+              <select
+                id="options"
+                style={{
+                  border: "1px solid black",
+                  padding: "5px 5px 5px 0px",
+                  borderRight: "none",
+                  paddingRight: "10px",
+                  borderTopLeftRadius: "7px",
+                  borderBottomLeftRadius: "7px",
+                  marginLeft: ".5rem",
+                }}
+              >
+                <option value="" hidden></option>
+                <option value="option1">Option 1</option>
+                <option value="option2">Option 2</option>
+                <option value="option3">Option 3</option>
+              </select>
+              <span
+                style={{
+                  position: "absolute",
+                  right: "0",
+                  left: "100%",
+                  pointerEvents: "none",
+                  background: "white",
+                  padding: "3px 0",
+                  paddingLeft: ".2rem",
+                  paddingRight: "1.2rem",
+                  border: "1px solid black",
+                  borderTopRightRadius: "7px",
+                  borderBottomRightRadius: "7px",
+                }}
+              >
+                %
+              </span>
+            </div>
+
+            <div>
+              <input
+                type="radio"
+                id="option1"
+                name="option"
+                value="option1"
+                checked
+              />
+            </div>
+          </div>
+          <div className="flexRows">
+            <label className="label" htmlFor="Overtime">
+              Overtime
+            </label>
+
+            <div>
+              <input
+                type="radio"
+                id="option1"
+                name="option"
+                value="option1"
+                style={{
+                  marginLeft: "10.7rem",
+                }}
+                checked
+              />
+            </div>
+          </div>
+
+          <div className="flexRows">
+            <label className="label" htmlFor="Bonus">
+              Bonus
+            </label>
+
+            <div>
+              <input
+                type="radio"
+                id="option1"
+                name="option"
+                value="option1"
+                style={{
+                  marginLeft: "12.3rem",
+                }}
+                checked
+              />
+            </div>
+          </div>
+
+          <div className="flexRows">
+            <label className="label" htmlFor="Other">
+              Other
+            </label>
+
+            <div>
+              <input
+                type="radio"
+                id="option1"
+                name="option"
+                value="option1"
+                style={{
+                  marginLeft: "12.5rem",
+                }}
+                checked
+              />
+            </div>
+          </div>
+        </form>
+        <br></br>
+        <br></br>
+        <h2 style={{ color: "#4E57BB", fontSize: "2rem", fontWeight: "600" }}>
+          Deductions
+        </h2>
+        <form>
+          <div className="flexRows">
+            <label className="label">Employer Pension Contribution</label>
+            <div className="inputDivs">
+              <BaseInput
+                type="number"
+                className="basic"
+                defaultValue="10"
+                style={{
+                  border: "1px solid black",
+                  padding: "0",
+                  borderRadius: "7px",
+                  width: "4rem",
+                  height: "1.5rem",
+                  marginLeft: "2.3rem",
+                  color: "black",
+                }}
+              />
+              <span
+                style={{
+                  position: "absolute",
+                  left: "42%",
+                  pointerEvents: "none",
+                  background: "white",
+                  padding: ".1rem",
+                  border: "1px solid black",
+                  borderRight: "none",
+                }}
+              >
+                %
+              </span>
+            </div>
+            <div>
+              <BaseInput
+                type="radio"
+                id="option1"
+                name="option"
+                value="option1"
+                style={{
+                  marginLeft: "1.4rem",
+                }}
+                checked
+              />
+            </div>
+          </div>
+
+          <div className="flexRows">
+            <label className="label">Employee Pension Contribution</label>
+            <div className="inputDivs">
+              <BaseInput
+                type="number"
+                className="basic"
+                defaultValue="8"
+                style={{
+                  border: "1px solid black",
+                  padding: "0",
+                  borderRadius: "7px",
+                  width: "4rem",
+                  height: "1.5rem",
+                  marginLeft: "2.3rem",
+                }}
+              />
+              <span
+                style={{
+                  position: "absolute",
+                  left: "42%",
+                  pointerEvents: "none",
+                  background: "white",
+                  padding: ".1rem",
+                  border: "1px solid black",
+                  borderRight: "none",
+                }}
+              >
+                %
+              </span>
+            </div>
+
+            <div>
+              <input
+                type="radio"
+                id="option1"
+                name="option"
+                value="option1"
+                style={{
+                  marginLeft: "1.2rem",
+                }}
+                checked
+              />
+            </div>
+          </div>
+
+          <div className="flexRows">
+            <label className="label" htmlFor="Transport">
+              PAYE
+            </label>
+
+            <div>
+              <input
+                type="radio"
+                id="option1"
+                name="option"
+                value="option1"
+                style={{
+                  marginLeft: "26rem",
+                }}
+                checked
+              />
+            </div>
+          </div>
+          <div className="flexRows">
+            <label className="label" htmlFor="Transport">
+              Others
+            </label>
+
+            <div>
+              <input
+                type="radio"
+                id="option1"
+                name="option"
+                value="option1"
+                style={{
+                  marginLeft: "25.2rem",
+                }}
+                checked
+              />
+            </div>
+          </div>
+          <BaseButton
+            type="submit"
+            backgroundcolor={"#4E57BB"}
+            width={"fit-content"}
+            style={{
+              marginLeft: "75%",
+            }}
+          >
+            Save
+          </BaseButton>
+        </form>
       </PayrollSettingsWrapper>
     </Layout>
   );
--- src/containers/dashboard/payrollsettings/styled.js
+++ src/containers/dashboard/payrollsettings/styled.js
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
-export const PayrollSettingsWrapper = styled("div")(() => {
+export const PayrollSettingsWrapper = styled("row")(() => {
   return {
     // Newton your CSS should go in below this line
     ".flexRows": {
@@ -8,6 +8,41 @@ export const PayrollSettingsWrapper = styled("div")(() => {
       flexDirection: "row",
       alignItems: "center",
       gap: "5rem",
+    },
+    "& form": {
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "flex-start",
+      gap: "1rem",
+    },
+    "&.inputDivs": {
+      position: "relative",
+      display: "flex",
+      alignItems: "center",
+    },
+    "&.basic": {
+      border: "1px solid black",
+      padding: "0",
+      borderRadius: "7px",
+      width: "4rem",
+    },
+    "&.perSpan": {
+      position: "absolute",
+      left: "17.5%",
+      pointerEvents: "none",
+      background: "white",
+      padding: ".1rem",
+      border: "1px solid black",
+      borderRight: "none",
+    },
+    "& .label": {
+      fontWeight: "600",
+      fontSize: "1.1rem",
+      lineHeight: "2rem",
+      color: "#000000",
+    },
+    "& #option1": {
+      transform: "scale(2)",
     },
   };
 });
