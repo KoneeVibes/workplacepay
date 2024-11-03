d402dd4eae682f82af349b61c851cd11ad365d11
Newton <ephraimnewton533@gmail.com>
2024-11-03 12:21:48 +0100

    pension page

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import { GetStarted } from "./pages/preliminaryauth/getstarted";
 import { Variance } from "./pages/app/variancereport";
 import { GeneralReport } from "./pages/app/generalreport";
 import { Paye } from "./pages/app/paye";
+import { Pension } from "./pages/app/pension";
 import { Summary } from "./pages/app/summary";
 import { UserSummary } from "./pages/app/usersummary";
 
@@ -35,6 +36,7 @@ function App() {
         <Route path="/reportsummary/variance" element={<Variance />} />
         <Route path="/reportsummary/general" element={<GeneralReport />} />
         <Route path="/reportsummary/payeoutput" element={<Paye />} />
+        <Route path="/reportsummary/pensionoutput" element={<Pension />} />
         <Route path="/reportsummary/summary" element={<Summary />} />
         <Route path="/reportsummary/summary/:id" element={<UserSummary />} />
       </Routes>
