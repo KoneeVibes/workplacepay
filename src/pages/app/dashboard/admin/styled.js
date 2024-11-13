e93e225633611cb1a06adbe79e32af7005b5fb31
Newton <ephraimnewton533@gmail.com>
2024-11-13 15:07:25 +0100

    admin dashboard creation

--- src/pages/app/dashboard/admin/styled.js
+++ src/pages/app/dashboard/admin/styled.js
@@ -3,5 +3,50 @@ import styled from "styled-components";
 export const AdminDashboardWrapper = styled("div")(() => {
     return {
         // Newton, your code goes in here 
-    }
-})
+    borderRadius: "1rem",
+    backgroundColor: "#FFFFFF",
+    "& select": {
+      padding: "calc(var(--cardPadding)/2)",
+      borderRadius: "10px",
+    },
+    "& fieldset": {
+      flex: 1,
+      overflow: "hidden",
+    },
+    "& label": {
+      fontFamily: "Poppins",
+      fontWeight: 600,
+      fontSize: 20,
+      color: "#000000",
+    },
+    "& .heading-row": {
+      padding: "var(--cardPadding)",
+      marginBlockEnd: "var(--sectionMargin)",
+      "& span": {
+        fontFamily: "Poppins",
+        fontWeight: 500,
+        fontSize: 18,
+        color: "#000000",
+      },
+    },
+    "& .filter": {
+      padding: "0 var(--cardPadding)",
+      justifyContent: "space-between",
+      marginBlockEnd: "var(--sectionMargin)",
+      "@media screen and (max-width: 768px)": {
+        flexDirection: "column",
+      },
+    },
+    "& .admin-table": {
+      overflow: "auto",
+    },
+    "& th": {
+      color: "#FFFFFF",
+      background: "#4E57BB",
+      minWidth: "200px",
+    },
+    "& td": {
+      minWidth: "200px",
+    },
+  };
+});
