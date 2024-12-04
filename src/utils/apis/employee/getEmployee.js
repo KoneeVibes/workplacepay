2c7b7704268ae2e71e63a5707180c46a0c76dfdf
Newton <ephraimnewton533@gmail.com>
2024-12-04 11:18:30 +0100

    get only one employee request

---
+++ src/utils/apis/employee/getEmployee.js
@@ -0,0 +1,25 @@
+import { BASE_ENDPOINT } from "../../endpoint";
+
+export const getEmployee = async (employeeId) => {
+  try {
+    const response = await fetch(
+      `${BASE_ENDPOINT}/api/users/employees/${companyId}/${employeeId}
+`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    const res = await response.json();
+    if (!response.ok) {
+      console.error("Error:", res);
+      throw new Error(res.message);
+    }
+    return res.data;
+  } catch (error) {
+    console.error("API fetch error:", error);
+    throw error;
+  }
+};
