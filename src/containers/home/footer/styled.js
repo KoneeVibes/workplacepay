95eefb5c01fb551c1f629a9f60d14cc56f93ce02
Konee <koneevibes@gmail.com>
2025-04-11 13:11:55 +0100

    resolving conflict in footer

--- src/containers/home/footer/styled.js
+++ src/containers/home/footer/styled.js
@@ -22,13 +22,15 @@ export const FooterWrapper = styled(Row)(() => {
       flex: 1,
       padding: "var(--cardPadding) 0",
       gap: "0",
+    },
+
     ".contact": {
       flex: 1,
       padding: "var(--cardPadding) 0",
       gap: "0",
     },
 
-     ".emailInput": {
+    ".emailInput": {
       padding: "10px",
     },
 
@@ -40,5 +42,5 @@ export const FooterWrapper = styled(Row)(() => {
     "@media screen and (max-width: 1280px)": {
       gap: "calc(var(--flexGap) * 3)",
     },
-  };
+  }
 });
