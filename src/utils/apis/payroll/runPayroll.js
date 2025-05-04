7135d88416b09076d4e410d591f6b8e34363bf9d
Konee <104918313+KoneeVibes@users.noreply.github.com>
2025-05-05 00:38:49 +0100

    recreated runPayroll.js file to fix deployment issue

---
+++ src/utils/apis/payroll/runPayroll.js
@@ -0,0 +1,23 @@
+import { BASE_ENDPOINT } from "../../endpoint";
+
+export const runPayrollService = async (TOKEN, companyId, payload) => {
+    try {
+        const response = await fetch(`${BASE_ENDPOINT}/api/payrolls/${companyId}/run`, {
+            method: 'POST',
+            headers: {
+                'Authorization': `Bearer ${TOKEN}`,
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(payload)
+        });
+        const res = await response.json();
+        if (!response.ok) {
+            console.error('Error:', res);
+            throw new Error(res.message);
+        }
+        return res.data;
+    } catch (error) {
+        console.error('API fetch error:', error);
+        throw error;
+    }
+};
